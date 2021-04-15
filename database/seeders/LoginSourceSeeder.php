9b8f298048d649fe8ae81022d44e4bd45da0a14a
torsunovAlexandr <40315182+torsunovAlexandr@users.noreply.github.com>
2021-04-15 16:19:33 +0500

    Поправил название класса сидера

--- database/seeders/LoginSourceSeeder.php
+++ database/seeders/LoginSourceSeeder.php
@@ -6,7 +6,7 @@ use Carbon\Carbon;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
-class loginSourceSeeder extends Seeder
+class LoginSourceSeeder extends Seeder
 {
     /**
      * Run the database seeds.
